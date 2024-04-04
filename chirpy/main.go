165f8851d4c1e8214920fb629199ca14b4fe1793
Simon Pitt <si_pitt@yahoo.com>
2024-04-04 23:29:54 +0100

    Add router with stateful handlers

--- chirpy/main.go
+++ chirpy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -8,10 +9,15 @@ import (
 func main() {
 
 	mux := http.NewServeMux()
-	filepathRoot := "."
-	fs := http.FileServer(http.Dir(filepathRoot))
 
-	mux.Handle("/", fs)
+	filepathRoot := "."
+	fsHandle := http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
+	var apiConfig apiConfig
+	mux.Handle("/app/*", apiConfig.middlewareMetricsInc(fsHandle))
+
+	mux.HandleFunc("/healthz", healthEndPoint)
+	mux.HandleFunc("/metrics", apiConfig.getFsHits)
+	mux.HandleFunc("/reset", apiConfig.resetFsHits)
 
 	corsMux := middlewareCors(mux)
 	port := ":8080"
@@ -22,6 +28,37 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache")
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getFsHits(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) resetFsHits(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits reset to zero."))
+}
+
+func healthEndPoint(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
 
 func middlewareCors(next http.Handler) http.Handler {
